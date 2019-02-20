fb4199b27f823555b28eabb45188b3242936bd4b
jmleroux <jmleroux.pro@gmail.com>
2019-02-21 00:34:54 +0100

    #17 - First naive queries

---
+++ tests/Integration/Query/BranchLastBuildTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jmleroux\CircleCi\Tests\Integration\Query;
+
+use Jmleroux\CircleCi\Model\Job;
+use PHPUnit\Framework\TestCase;
+
+class BranchLastBuildTest extends TestCase
+{
+    public function testCreateFromJson()
+    {
+        require dirname(__DIR__) . '/../../vendor/autoload.php';
+
+        $query = new \Jmleroux\CircleCi\Query\BranchLastBuild(
+            'c900a267b73d8fbcab665fedc818c8de2b6aedf1',
+            'github',
+            'jmleroux',
+            'circleci-php-client'
+        );
+
+        $build = $query->execute('master');
+
+        $this->assertInstanceOf(Job::class, $build);
+    }
+}
