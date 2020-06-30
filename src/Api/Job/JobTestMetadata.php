86b8615c1f74e268de21e01089df38c02f031894
Benoit Jacquemont <benoit@akeneo.com>
2020-06-30 10:32:02 +0200

    Add tests metadata retrieval

---
+++ src/Api/Job/JobTestMetadata.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jmleroux\CircleCi\Api\Job;
+
+use Jmleroux\CircleCi\Client;
+use Jmleroux\CircleCi\Model\TestMetadata;
+use Jmleroux\CircleCi\ValidateClientVersionTrait;
+
+/**
+ * Get a job test metadata
+ * Returns a sequence of test metadata for a job
+ *
+ * @author Benoit Jacquemont <benoit@akeneo.com>
+ * @link https://circleci.com/docs/api/v2/#get-test-metadata
+ */
+class JobTestMetadata
+{
+    use ValidateClientVersionTrait;
+
+    /** @var Client */
+    private $client;
+
+    public function __construct(Client $client)
+    {
+        $this->validateClientVersion($client, ['v2']);
+        $this->client = $client;
+    }
+
+    /**
+     * @return TestMetadata[]
+     */
+    public function execute(string $projectSlug, int $jobNumber): array
+    {
+        $testMetadata = [];
+        $uri = sprintf('project/%s/%s/tests', $projectSlug, $jobNumber);
+        $params = [];
+
+        $nextPageToken = null;
+        do {
+            if (null !== $nextPageToken) {
+                $params['page-token'] = $nextPageToken;
+            }
+
+            $response = json_decode((string) $this->client->get($uri, $params)->getBody());
+            $nextPageToken = $response->next_page_token;
+
+            foreach ($response->items as $item) {
+                $testMetadata[] = TestMetadata::createFromApi($item);
+            }
+        } while (null !== $nextPageToken);
+
+        return $testMetadata;
+    }
+}
---
+++ src/Model/TestMetadata.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jmleroux\CircleCi\Model;
+
+use DateTimeImmutable;
+
+/**
+ * @author Benoit Jacquemont <benoit@akeneo.com>
+ * @link https://circleci.com/docs/api/v2/#get-test-metadata
+ */
+class TestMetadata implements ApiResultInterface
+{
+    /**
+     * Raw object from Circle CI API
+     *
+     * @var \stdClass
+     */
+    private $rawObject;
+
+    private function __construct(\stdClass $rawObject)
+    {
+        $this->rawObject = $rawObject;
+    }
+
+    public static function createFromApi(\stdClass $rawObject): self
+    {
+        return new self($rawObject);
+    }
+
+    public function rawValues(): \stdClass
+    {
+        return $this->rawObject;
+    }
+
+    public function message(): string
+    {
+        return $this->rawObject->message;
+    }
+
+    public function source(): string
+    {
+        return $this->rawObject->source;
+    }
+
+    public function runTime(): float
+    {
+        return $this->rawObject->run_time;
+    }
+
+    public function file(): string
+    {
+        return $this->rawObject->file;
+    }
+
+    public function result(): string
+    {
+        return $this->rawObject->result;
+    }
+
+    public function name(): string
+    {
+        return $this->rawObject->name;
+    }
+
+    public function classname(): string
+    {
+        return $this->rawObject->classname;
+    }
+}
